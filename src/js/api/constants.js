1c8075734a98acdb3a9d7987393758d57f54c836
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-26 19:35:37 +0200

    added api key

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,6 @@
 // Use Postman, or JavaScript to get your API key
 // In Workflow we will learn how to secure this information
-export const API_KEY = "replace-with-your-key";
+export const API_KEY = "056bc316-c172-4962-8175-268e2fa3db6b";
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
