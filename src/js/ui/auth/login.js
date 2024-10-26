8d386d9d150aeef53c24c80b11a414c38a8215e9
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-26 19:37:20 +0200

    setup initial login functionality using the login function from /api/auth/login

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,5 +1,24 @@
+import { login } from "../../api/auth/login";
+// import router from "../../router";
+
 /**
  * This function should pass data to the login function in api/auth and handle the response
  */
 
-export async function onLogin(event) {}
+export async function onLogin(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+        const userData = await login(data);
+        const { accessToken, ...profile } = userData.data;
+        
+        console.log(accessToken)
+        console.log(profile)
+
+    } catch (error) {
+        console.error('Whoops! An error occurred during login: ', error);
+    }
+}
