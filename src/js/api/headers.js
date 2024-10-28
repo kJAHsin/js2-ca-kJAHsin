3f25bbba0545f8d31a56d1ae715f4b10062bcff3
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-28 13:45:06 +0100

    added JWT to headers function

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,10 +1,18 @@
-import { API_KEY } from "./constants";
+import { API_KEY } from "./constants.js";
+import { loadData } from "./storage/load.js";
 
 export function headers() {
   const headers = new Headers();
+  const accessToken = loadData('accessToken');
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
+  }
+
+  if (accessToken) {
+    headers.append("Authorization", accessToken);
+  } else {
+    console.error('WHOOPSIE! No access token present.')
   }
 
   return headers;
