1d37f077ec6accdc9ee52d7b79b8bcc5864d8b4a
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-28 16:10:17 +0100

    working on functionality after login

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,3 +1,11 @@
-import { authGuard } from "../../utilities/authGuard";
+import { loadData } from "../../api/storage";
+import { authGuard } from "../../utilities/authGuard.js";
 
 authGuard();
+
+const userData = loadData('profile');
+
+console.log(userData)
+
+// not working
+// window.location.href = `/profile/${userData.name}`
