d59a1ceaa1767d0869d61629f290ff012f19e207
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-28 12:02:16 +0100

    just a small typo

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -2,7 +2,7 @@
  * Deletes a post by its ID.
  *
  * @param {?} id - The ID of the post to delete. Fill in the appropriate type.
- * @returns {?} What the fucntion returns. Choose an appropriate return type.
+ * @returns {?} What the function returns. Choose an appropriate return type.
  * @throws {Error} If the API request fails.
  */
 export async function deletePost(id) {}
