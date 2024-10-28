3cda8b87a0ed4ba83a65c56450a09b2ccc3b2ab1
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-28 16:10:42 +0100

    tweaked a little bit for more understandable semaphore name

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,5 +1,10 @@
+import { loadData } from "../api/storage";
+
 export function authGuard() {
-  if (!localStorage.token) {
+  const accessToken = loadData('accessToken');
+  console.log(accessToken)
+
+  if (!accessToken) {
     alert("You must be logged in to view this page");
     window.location.href = "/auth/login/";
   }
