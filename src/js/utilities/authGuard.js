c7dd7f0151a6863f568ea342c96758c7eb92aac5
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-29 09:26:36 +0100

    working with debugging

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -2,7 +2,6 @@ import { loadData } from "../api/storage";
 
 export function authGuard() {
   const accessToken = loadData('accessToken');
-  console.log(accessToken)
 
   if (!accessToken) {
     alert("You must be logged in to view this page");
