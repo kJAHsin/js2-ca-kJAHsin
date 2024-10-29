8bad3eff876908c62b6478958e2f4579099f196f
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-29 10:42:24 +0100

    profile page templating

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -2,7 +2,38 @@ import { loadData } from "../../api/storage";
 import { authGuard } from "../../utilities/authGuard.js";
 
 authGuard();
+// profile template elements
+const profile = document.getElementById('profile-info');
+const profileHeader = document.getElementById('profile-header');
+const profileAvatar = document.getElementById('profile-avatar');
 
-const userData = loadData('profile');
+// setting local storage info
+const userDataStorage = loadData('profile');
+const {name, email, bio, avatar, banner} = userDataStorage;
+const profileData = {name, email, bio, avatar, banner};
 
-console.log(userData)
+console.log(userDataStorage)
+
+function createProfileHTML() {
+    // creating elements
+    const pName = document.createElement('h2');
+    const pEmail = document.createElement('p');
+    const pBio = document.createElement('p');
+    const pAvatarImg = document.createElement('img');
+
+    pName.textContent = profileData.name;
+    pEmail.textContent = profileData.email;
+    pBio || pBio !== null
+        ? pBio.textContent = profileData.bio
+        : pBio.textContent = '';
+    pAvatarImg.src = profileData.avatar.url;
+    pAvatarImg.alt = profileData.avatar.alt;
+    profileHeader.style.backgroundImage = `url(${profileData.banner.url})`;
+
+    profile.appendChild(pName);
+    profile.appendChild(pEmail);
+    profile.appendChild(pBio);
+    profileAvatar.appendChild(pAvatarImg);
+}
+
+createProfileHTML();
