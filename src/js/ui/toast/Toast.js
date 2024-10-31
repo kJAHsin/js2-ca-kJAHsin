61b925c51d990c69a036242b09e2fae322faf7ae
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-31 17:29:45 +0100

    implemented class switching as opposed to styling in js

--- src/js/ui/toast/Toast.js
+++ src/js/ui/toast/Toast.js
@@ -7,20 +7,10 @@ export class Toast {
         this.toastEl = null;
     }
 
-    setColor() {
-        switch (this.type) {
-            case 'success':
-                return 'green';
-            case 'warning':
-                return 'red';
-        }
-    }
-
     createToast() {
         const newToast = document.createElement('div');
-        newToast.className = 'toast';
+        newToast.className = `toast ${this.type}`;
         newToast.innerHTML = `<p>${this.text}</p>`;
-        newToast.style.backgroundColor = this.setColor();
         document.body.appendChild(newToast);
         this.toastEl = newToast
     }
