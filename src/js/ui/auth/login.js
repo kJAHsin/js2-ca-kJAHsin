efd4695a2eabc87cd68de1dcda47300fbb2dbcd1
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-31 16:22:40 +0100

    replaced alerts with custom Toast

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,6 +1,7 @@
 import { login } from "../../api/auth/login";
 import { saveData } from "../../api/storage";
 import router from "../../router";
+import { Toast } from "../toast/Toast.js";
 
 /**
  * This function should pass data to the login function in api/auth and handle the response
@@ -15,14 +16,19 @@ export async function onLogin(e) {
     try {
         const userData = await login(data);
         const { accessToken, ...profile } = userData.data;
+        const toast = new Toast('success', `You were successfully logged in as<br>${profile.name}!`);
+        toast.toastIt();
         
         saveData('accessToken', accessToken);
         saveData('profile', profile);
 
-        alert(`You were successfully logged in as ${profile.name}!`);
-        router('/profile/');
-        window.location.href = '/profile/';
+        setTimeout(() => {
+            router('/profile/');
+            window.location.href = '/profile/';
+        }, 5000);
     } catch (error) {
         console.error('Whoops! An error occurred in the onlogin() function: ', error);
+        const toast = new Toast('warning', `Sorry. We couldn't sign you in!`);
+        toast.toastIt();
     }
 }
