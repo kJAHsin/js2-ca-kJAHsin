df4f18b3129acb3abebac10655912765efe61b3d
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-10-31 16:23:46 +0100

    replaced alert with custom Toast and added safeguards to avoid user clicking anything on the page when being redirected to login page

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,10 +1,14 @@
 import { loadData } from "../api/storage";
+import { Toast } from "../ui/toast/Toast.js";
 
 export function authGuard() {
   const accessToken = loadData('accessToken');
 
   if (!accessToken) {
-    alert("You must be logged in to view this page");
-    window.location.href = "/auth/login/";
+    document.body.style.pointerEvents = 'none';
+    const toast = new Toast('warning', 'No access!<br>This page is only for logged in users, Friend!<br>Please log in and try again.');
+    toast.toastIt();
+    
+    setTimeout(() => {window.location.href = "/auth/login/"}, 5000);
   }
 }
