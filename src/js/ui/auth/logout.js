f1e238fdbffdc592a35c4b50aa7a3c4a638bf20c
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-02 13:49:18 +0100

    added toast and automatic navigation to next page

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,4 +1,5 @@
 import { clearData } from "../../api/storage";
+import { Toast } from "../toast/Toast.js";
 
 /**
  * This function should log the user out by removing aproppriate user data from the browser.
@@ -12,8 +13,14 @@ export function onLogout() {
       clearData(item);
     } catch (err) {
       console.error(
-        `The item you are trying to remove is not present: \n\t ${err}`
+        `The item you are trying to remove is not present: \n\t${item} \n\t${err}`
       );
     }
+    const toast = new Toast('success', 'You were successfully logged out!');
+    toast.toastIt();
+
+    setTimeout(() => {
+      window.location.href = '/auth/login/';
+    }, 3000);
   });
 }
