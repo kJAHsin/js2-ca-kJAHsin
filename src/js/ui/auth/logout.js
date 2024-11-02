8eb7a9805c45ac53174d9c79171b42316fcaddca
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-02 11:45:51 +0100

    function set up to remove localStorage keys

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,5 +1,19 @@
+import { clearData } from "../../api/storage";
+
 /**
  * This function should log the user out by removing aproppriate user data from the browser.
  */
 
-export function onLogout() {}
+export function onLogout() {
+  const storageItems = ["accessToken", "profile"];
+
+  storageItems.forEach((item) => {
+    try {
+      clearData(item);
+    } catch (err) {
+      console.error(
+        `The item you are trying to remove is not present: \n\t ${err}`
+      );
+    }
+  });
+}
