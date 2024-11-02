8c5b9a278d8bf66b7dee34878a81627a23d926ce
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-02 13:50:24 +0100

    set up to pass the shadow element to the event listener to ensure that it is only getting set once and only if the element is in the dom.

--- src/js/ui/global/components/LogoutButton.js
+++ src/js/ui/global/components/LogoutButton.js
@@ -1,13 +1,53 @@
+import { loadData } from "../../../api/storage/load.js";
 import { setLogoutListener } from "../logout.js";
 
-export class LogoutButton {
-    constructor (text, style) {
-        this.text = text;
-        this.style = style;
+export class LogoutButton extends HTMLElement {
+  constructor() {
+    super();
+  }
 
-        this.setListener();
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+    const button = document.createElement("button");
+    button.id = "logOutBtn";
+    button.className = "btn logout-btn";
+    button.innerText = "Log out";
+
+    // attaching stylesheet
+    const style = this.getStyleSheet();
+
+    shadow.appendChild(style);
+    shadow.appendChild(button);
+    this.setListener();
+    this.checkLoggedin(button);
+  }
+
+  // attach listener for logout function
+  setListener() {
+    const btn = this.shadowRoot.getElementById("logOutBtn");
+
+    if (btn) {
+      setLogoutListener(btn);
     }
+  }
 
-    // attach listener for logout function
-    setListener = () => setLogoutListener();
+  //   import stylesheet
+  getStyleSheet() {
+    const link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", "../../src/css/components/btn.css");
+
+    return link;
+  }
+
+  //   check if user is logged in and show log out button if so
+  checkLoggedin(el) {
+    const token = loadData("accessToken");
+
+    if (!token) {
+      el.classList.add("hidden");
+    }
+  }
 }
+
+customElements.define("logout-button", LogoutButton);
--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -3,10 +3,8 @@ import { onLogout } from "../auth/logout.js";
 /**
  * Functions you attach to logout events that calls ui/auth/logout function
  */
-export function setLogoutListener() {
-    const logOutBtn = document.getElementById('logOutBtn');
-
-    logOutBtn.addEventListener('click', (e) => {
+export function setLogoutListener(el) {
+    el.addEventListener('click', (e) => {
         e.preventDefault();
         onLogout();
     })
