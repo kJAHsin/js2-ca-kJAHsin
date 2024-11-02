ae70d65e6910d0c587f6e62ec229b05cb2a22469
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-02 13:48:36 +0100

    removed authGuard functionality from home page.  no reason to navigate automatically away from here

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,3 +1,10 @@
-import { authGuard } from "../../utilities/authGuard";
+import { loadData } from "../../api/storage";
 
-authGuard();
+const profileLink = document.getElementById('profileLink');
+
+const token = loadData('accessToken');
+
+if (Boolean(!token)) {
+    profileLink.ariaDisabled = true;
+    profileLink.classList.add('disabled'); 
+}
