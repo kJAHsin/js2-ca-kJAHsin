e1eda086f9abff5d0a162f4989b641bbe6b34746
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-02 11:46:05 +0100

    setting listener on log out button

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,4 +1,13 @@
+import { onLogout } from "../auth/logout.js";
+
 /**
  * Functions you attach to logout events that calls ui/auth/logout function
  */
-export function setLogoutListener() {}
+export function setLogoutListener() {
+    const logOutBtn = document.getElementById('logOutBtn');
+
+    logOutBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        onLogout();
+    })
+}
