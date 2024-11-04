b215192c4b1a3d311125c19f63b08c8f2fd95fee
kJAHsin <kjahnsencollins@gmail.com>
2024-11-04 19:34:50 +0100

    added function to handle logic.  now need to create html form to take in information

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,5 +1,30 @@
+import { createPost } from "../../api/post/create.js";
+
 /**
- * Passses data to the createPost function in api/post and handles the response
+ * Passes data to the createPost function in api/post and handles the response
  */
 
-export async function onCreatePost(event) {}
+export async function onCreatePost(e) {
+    e.preventDefault();
+
+    // const formData = new FormData(e.target);
+    // const data = Object.fromEntries(formData.entries());
+
+    const data = {
+        title: 'Test post with beans on it!',
+        body: 'This is the body with some text for testing.',
+    }
+
+    try {
+        const response = await createPost(data);
+        if (response) {
+            // Handle succcessful creation of post
+            console.log(`Way to go! You created the post ${data.title}`);
+            // TO DO
+            // Add toast and redirect
+        }
+    } catch (err) {
+        console.error('Could not create post', err);
+    }
+
+}
