4aac96b4157a6e7668c2c56c5f801db37c973ede
kJAHsin <kjahnsencollins@gmail.com>
2024-11-09 09:16:20 +0100

    added toast and redirect to logout

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,11 +1,24 @@
-import { onLogout } from "../auth/logout.js";
+import { onLogout } from '../auth/logout.js'
+import { Toast } from '../toast/Toast.js'
 
 /**
  * Functions you attach to logout events that calls ui/auth/logout function
  */
 export function setLogoutListener(el) {
-    el.addEventListener('click', (e) => {
-        e.preventDefault();
-        onLogout();
-    });
-};
+	el.addEventListener('click', (e) => {
+		e.preventDefault()
+		onLogout()
+
+		// Handle successful creation of post
+		const toast = new Toast(
+			'success',
+			`You have now been logged out!`
+		)
+		toast.toastIt()
+
+		toast.toastEl.addEventListener(
+			'animationend',
+			() => (window.location.href = '/')
+		)
+	})
+}
