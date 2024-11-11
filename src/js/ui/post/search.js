a10246517516ea1ef6ca5312195d7ab581e60635
kJAHsin <kjahnsencollins@gmail.com>
2024-11-11 18:37:46 +0100

    added PostCard instance call to post page

--- src/js/ui/post/search.js
+++ src/js/ui/post/search.js
@@ -1,7 +1,8 @@
 import { readPost } from '../../api/post/read.js'
+import { PostCard } from '../global/components/PostCard.js';
 import { Toast } from '../toast/Toast.js'
 
-export async function onSearchPost() {
+export async function onSearchPost(e) {
     e.preventDefault();
 
     const formData = new FormData(e.target)
@@ -10,12 +11,15 @@ export async function onSearchPost() {
     try {
 		const response = await readPost(data)
 		if (response) {
+			const {author, body, id, media, title} = response	
 			// Handle successful creation of post
 			const toast = new Toast(
 				'success',
-				`Way to go! You found the post ${response}`
+				`Way to go! You found the post ${title}`
 			)
 			toast.toastIt()
+			const card = new PostCard(author, id, title, body, media)
+			card.renderCard()
 		}
 	} catch (err) {
 		console.error('Could not retrieve post', err)
