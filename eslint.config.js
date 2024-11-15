f87806151a3f32175863bf9a85d27f6470614923
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-15 05:36:07 +0100

    update branch eslint, prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,23 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
 export default [
-    {
-        rules: {
-            semi: "error",
-            "prefer-const": "error"
-        },
-        ignores: [
-            '**/node_modules/**',
-            '**/dist/**'
-        ]
-    }
-];
+   {
+      rules: {
+         ...js.configs.recommended.rules,
+      },
+      ignores: ['**/node_modules/**', '**/dist/**'],
+   },
+   {
+      files: ['**/*.js'],
+      languageOptions: {
+         ecmaVersion: 2020,
+         globals: globals.browser,
+         parserOptions: {
+            ecmaVersion: 'latest',
+            ecmaFeatures: {},
+            sourceType: 'module',
+         },
+      },
+   },
+]
--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,29 +1,28 @@
-import { register } from '../../api/auth/register';
-import router from '../../router';
+import { register } from '../../api/auth/register'
 
 /**
  * This function should pass data to the register function in api/auth and handle the response
  */
 
 export async function onRegister(e) {
-    e.preventDefault();
+   e.preventDefault()
 
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
+   const formData = new FormData(e.target)
+   const data = Object.fromEntries(formData.entries())
 
-    try {
-        const response = await register(data);
-        if (response) {
-            // Handle successful registration (e.g., redirect to login page)
-            console.log('Registration successful');
-            console.log(`User info: ${response}`);
-            // TO DO
-            // Add toast and redirect
-        } else {
-            // Handle registration failure (e.g., show error message)
-            console.error('Registration failed: ', response.message || response);
-        }
-    } catch (error) {
-        console.error('An error occurred during registration:', error);
-    }
+   try {
+      const response = await register(data)
+      if (response) {
+         // Handle successful registration (e.g., redirect to login page)
+         console.log('Registration successful')
+         console.log(`User info: ${response}`)
+         // TO DO
+         // Add toast and redirect
+      } else {
+         // Handle registration failure (e.g., show error message)
+         console.error('Registration failed: ', response.message || response)
+      }
+   } catch (error) {
+      console.error('An error occurred during registration:', error)
+   }
 }
