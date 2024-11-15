348f9106245227dc9171912ad2012c056865f649
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-15 18:19:04 +0100

    linting and added Toast with self-calling function

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,32 +1,37 @@
-import { login } from "../../api/auth/login";
-import { saveData } from "../../api/storage";
-import { Toast } from "../toast/Toast.js";
+import { login } from '../../api/auth/login'
+import { saveData } from '../../api/storage'
+import { Toast } from '../toast/Toast.js'
+import '../../../css/pages/login.css'
 
 /**
  * This function should pass data to the login function in api/auth and handle the response
  */
 
 export async function onLogin(e) {
-    e.preventDefault();
+   e.preventDefault()
 
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
+   const formData = new FormData(e.target)
+   const data = Object.fromEntries(formData.entries())
 
-    try {
-        const userData = await login(data);
-        const { accessToken, ...profile } = userData.data;
-        const toast = new Toast('success', `You were successfully logged in as<br>${profile.name}!`);
-        toast.toastIt();
-        
-        saveData('accessToken', accessToken);
-        saveData('profile', profile);
-        toast.toastEl.addEventListener('animationend', () => 
-            window.location.href = '/profile/'
-        );
+   try {
+      const userData = await login(data)
+      const { accessToken, ...profile } = userData.data
+      const toast = new Toast(
+         'success',
+         `You were successfully logged in as<br>${profile.name}!`,
+      )
 
-    } catch (error) {
-        console.error('Whoops! An error occurred in the onlogin() function: ', error);
-        const toast = new Toast('warning', `Sorry. We couldn't sign you in!`);
-        toast.toastIt();
-    }
+      saveData('accessToken', accessToken)
+      saveData('profile', profile)
+      toast.toastEl.addEventListener(
+         'animationend',
+         () => (window.location.href = '/profile/'),
+      )
+   } catch (error) {
+      console.error(
+         'Whoops! An error occurred in the onlogin() function: ',
+         error,
+      )
+      const toast = new Toast('warning', `Sorry. We couldn't sign you in!`)
+   }
 }
