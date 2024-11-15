694a4060888b2e02244bf6d0a42a72fefebe0f7e
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-15 07:50:59 +0100

    updated delete post error message

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,3 +1,6 @@
+import { API_SOCIAL_POSTS } from '../constants.js'
+import { headers } from '../headers.js'
+
 /**
  * Deletes a post by its ID.
  *
@@ -5,4 +8,22 @@
  * @returns {?} What the function returns. Choose an appropriate return type.
  * @throws {Error} If the API request fails.
  */
-export async function deletePost(id) {}
+export async function deletePost(id) {
+   try {
+      const response = await fetch(`${API_SOCIAL_POSTS}/${id}`, {
+         method: 'DELETE',
+         headers: headers(),
+      })
+      if (!response.ok) {
+         throw new Error(
+            `Error deleting post: (status: ok = ${response.ok})`,
+         )
+      }
+
+      console.log(response)
+      return response
+   } catch (err) {
+      console.error('There was a problem deleting this post: ', err)
+      throw err
+   }
+}
