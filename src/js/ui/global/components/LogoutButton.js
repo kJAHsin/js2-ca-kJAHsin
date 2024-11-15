e2e0e50d1ce4ba50f624c6c4320a4fed28ce56b8
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-15 17:34:58 +0100

    prettier formatting

--- src/js/ui/global/components/LogoutButton.js
+++ src/js/ui/global/components/LogoutButton.js
@@ -1,53 +1,56 @@
-import { loadData } from "../../../api/storage/load.js";
-import { setLogoutListener } from "../logout.js";
+import { loadData } from '../../../api/storage/load.js'
+import { setLogoutListener } from '../logout.js'
 
 export class LogoutButton extends HTMLElement {
-  constructor() {
-    super();
-  }
+   constructor() {
+      super()
+   }
 
-  connectedCallback() {
-    const shadow = this.attachShadow({ mode: "open" });
-    const button = document.createElement("button");
-    button.id = "logOutBtn";
-    button.className = "btn logout-btn";
-    button.innerText = "Log out";
+   connectedCallback() {
+      const shadow = this.attachShadow({ mode: 'open' })
+      const button = document.createElement('button')
+      button.id = 'logOutBtn'
+      button.className = 'btn logout-btn'
+      button.innerText = 'Log out'
 
-    // attaching stylesheet
-    const style = this.getStyleSheet();
+      // attaching stylesheet
+      const style = this.getStyleSheet()
 
-    shadow.appendChild(style);
-    shadow.appendChild(button);
-    this.setListener();
-    this.checkLoggedin(button);
-  }
+      shadow.appendChild(style)
+      shadow.appendChild(button)
+      this.setListener()
+      this.checkLoggedin(button)
+   }
 
-  // attach listener for logout function
-  setListener() {
-    const btn = this.shadowRoot.getElementById("logOutBtn");
+   // attach listener for logout function
+   setListener() {
+      const btn = this.shadowRoot.getElementById('logOutBtn')
 
-    if (btn) {
-      setLogoutListener(btn);
-    }
-  }
+      if (btn) {
+         setLogoutListener(btn)
+      }
+   }
 
-  //   import stylesheet
-  getStyleSheet() {
-    const link = document.createElement("link");
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("href", "../../src/css/components/btn.css");
+   //   import stylesheet
+   getStyleSheet() {
+      const link = document.createElement('link')
+      link.setAttribute('rel', 'stylesheet')
+      link.setAttribute(
+         'href',
+         '../../src/css/components/LogoutButton.css',
+      )
 
-    return link;
-  }
+      return link
+   }
 
-  //   check if user is logged in and show log out button if so
-  checkLoggedin(el) {
-    const token = loadData("accessToken");
+   //   check if user is logged in and show log out button if so
+   checkLoggedin(el) {
+      const token = loadData('accessToken')
 
-    if (!token) {
-      el.classList.add("hidden");
-    }
-  }
+      if (!token) {
+         el.classList.add('hidden')
+      }
+   }
 }
 
-customElements.define("logout-button", LogoutButton);
+customElements.define('logout-button', LogoutButton)
