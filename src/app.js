886dd605f01934f1278ba39297a1a359d365aa67
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-11-17 18:28:08 +0100

    fixed imports

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './css/style.css'
 import router from './js/router'
-import { LogoutButton } from './js/ui/global/components/LogoutButton.js'
-import NavBar from './js/ui/global/components/NavBar.js'
+import './js/ui/global/components/LogoutButton.js'
+import './js/ui/global/components/NavBar.js'
 
 await router(window.location.pathname)
