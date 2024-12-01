b7849413c8126bf394ea4bd6c9de0759679ee5c7
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-12-01 20:24:16 +0100

    added deletion logic to router logic

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,6 +1,14 @@
 import { onSearchPost } from '../../ui/post/search.js'
+import { onDeletePost } from '../../ui/post/delete.js'
 import '../../../css/pages/post.css'
 
 const form = document.forms.searchPost
+const deleteForm = document.forms.deletePost
+const openDeleteModalBtn = document.getElementById('open-delete-modal')
 
 form.addEventListener('submit', onSearchPost)
+deleteForm.addEventListener('submit', onDeletePost)
+openDeleteModalBtn.addEventListener('click', () => {
+   const deleteModal = document.getElementById('delete-modal')
+   deleteModal.classList.add('open')
+})
