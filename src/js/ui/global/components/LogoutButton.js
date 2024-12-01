22651f5c8acf0b8531e9de7328af13b8a0f7a732
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-12-01 16:32:12 +0100

    added styles directly to code for web components.  the build process was not working in production as desired.

--- src/js/ui/global/components/LogoutButton.js
+++ src/js/ui/global/components/LogoutButton.js
@@ -35,7 +35,91 @@ export class LogoutButton extends HTMLElement {
    getStyleSheet() {
       const style = document.createElement('style')
       style.textContent = `
-      @import url('/src/css/components/LogoutButton.css');
+      /* resets */
+      * {
+         &::before,
+         &::after,
+         & {
+            box-sizing: inherit;
+            padding: 0;
+            margin: 0;
+         }
+      }
+
+      body {
+         box-sizing: border-box;
+         margin: 0;
+         font-family: var(--font-fam);
+      }
+
+      a {
+         color: inherit;
+         text-decoration: none;
+      }
+
+      ul,
+      li {
+         list-style: none;
+      }
+
+      /* variables */
+      :root {
+         /* Font families */
+         --font-fam: system-ui, -apple-system, 'ubuntu', 'Open Sans',
+            'Helvetica Neue', sans-serif;
+         --font-link: 1.125rem;
+         --weight-link: 500;
+
+         /* Theme colors */
+         --bg-light: rgb(255 240 255);
+         --orange-400: rgb(255 186 117);
+         --orange-600: rgb(255 128 0);
+         --orange-800: rgb(209 104 0);
+         --blue-400: rgb(150 121 255);
+         --blue-600: rgb(91 46 255);
+         --blue-800: rgb(34 0 156);
+
+         /* layout */
+         --border-rad: 7px;
+         --border-width: 3px;
+         --spacing: calc(0.25rem + 2vw);
+
+         /* transition */
+         --trans-btn-hov: background 280ms ease-in, color 100ms ease-out 150ms;
+         --trans-btn-leave: background 120ms ease-in, color 50ms ease-out 50ms;
+      }
+
+      /* layout */
+      .logout-btn {
+         display: inline-block;
+         border-radius: var(--border-rad);
+         color: var(--orange-800);
+         padding: 0.75rem 2rem;
+         border: var(--border-width) solid var(--orange-800);
+         height: 100%;
+         font-size: var(--font-link);
+         font-weight: var(--weight-link);
+         background: linear-gradient(
+            80deg,
+            var(--bg-light) 40%,
+            var(--orange-800) 75%
+         );
+         background-size: 500%;
+         background-position-x: left;
+         transition: var(--trans-btn-leave);
+         cursor: pointer;
+
+         &:hover,
+         &:focus-visible {
+            background-position-x: right;
+            color: var(--bg-light);
+            transition: var(--trans-btn-hov);
+         }
+
+         &.hidden {
+            display: none;
+         }
+      }
       `
       return style
    }
--- src/js/ui/global/components/NavBar.js
+++ src/js/ui/global/components/NavBar.js
@@ -56,7 +56,110 @@ export default class NavBar extends HTMLElement {
    getStyleSheet() {
       const style = document.createElement('style')
       style.textContent = `
-      @import url('/src/css/components/NavBar.css');
+      /* resets */
+      * {
+         &::before,
+         &::after,
+         & {
+            box-sizing: inherit;
+            padding: 0;
+            margin: 0;
+         }
+      }
+
+      body {
+         box-sizing: border-box;
+         margin: 0;
+         font-family: var(--font-fam);
+      }
+
+      a {
+         color: inherit;
+         text-decoration: none;
+      }
+
+      ul,
+      li {
+         list-style: none;
+      }
+
+      /* variables */
+      :root {
+         /* Font families */
+         --font-fam: system-ui, -apple-system, 'ubuntu', 'Open Sans',
+            'Helvetica Neue', sans-serif;
+         --font-link: 1.125rem;
+         --weight-link: 500;
+
+         /* Theme colors */
+         --bg-light: rgb(255 240 255);
+         --orange-400: rgb(255 186 117);
+         --orange-600: rgb(255 128 0);
+         --orange-800: rgb(209 104 0);
+         --blue-400: rgb(150 121 255);
+         --blue-600: rgb(91 46 255);
+         --blue-800: rgb(34 0 156);
+
+         /* layout */
+         --border-rad: 7px;
+         --border-width: 3px;
+         --spacing: calc(0.25rem + 2vw);
+
+         /* transition */
+         --trans-btn-hov: background 280ms ease-in, color 100ms ease-out 150ms;
+         --trans-btn-leave: background 120ms ease-in, color 50ms ease-out 50ms;
+      }
+
+      /* layout */
+      .nav-bar {
+         display: flex;
+         justify-content: space-between;
+         padding-block: var(--spacing);
+         width: 100%;
+
+         img {
+            object-fit: contain;
+            width: calc(2rem + 6vw);
+            object-fit: contain;
+            filter: invert(1) brightness(0.25);
+            cursor: pointer;
+         }
+
+         ul.nav-links {
+            display: flex;
+            gap: var(--spacing);
+            align-items: center;
+
+            li.link {
+               border-radius: var(--border-rad);
+               border: var(--border-width) solid var(--blue-800);
+
+               a {
+                  display: inline-block;
+                  padding: 0.75rem 2rem;
+                  text-decoration: none;
+                  font-family: inherit;
+                  font-weight: var(--weight-link);
+                  font-size: var(--font-link);
+                  color: var(--blue-800);
+                  background-color: var(--bg-light);
+                  transition: var(--trans-btn-leave);
+
+                  &:hover,
+                  &:focus-visible {
+                     background-color: var(--blue-800);
+                     color: var(--bg-light);
+                     transition: var(--trans-btn-hov);
+                  }
+               }
+            }
+         }
+
+         logout-button {
+            height: 100%;
+         }
+      }
+
       `
       return style
    }
