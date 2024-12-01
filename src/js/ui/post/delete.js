b18fbf4857cc6d168ad2371ec79d6ffb01011c61
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-12-01 20:24:45 +0100

    changed deletion logic to happen inside of a modal

--- src/js/ui/post/delete.js
+++ src/js/ui/post/delete.js
@@ -1,11 +1,28 @@
 import { deletePost } from '../../api/post/delete.js'
+import { Toast } from '../toast/Toast.js'
+import { saveData } from '../../api/storage/save.js'
 /**
  * Passes data to the createPost function in api/post and handles the response
  */
 export async function onDeletePost(e) {
    e.preventDefault()
 
-   const id = e.target.dataset.postID
+   const formData = new FormData(e.target)
+   const data = Object.fromEntries(formData.entries())
+   const id = data.id
 
    deletePost(id)
+   closeModal()
+
+   new Toast(
+      'warning',
+      `Hope that wasn't a mistake hahaha. You've deleted post ${id}`,
+   )
+
+   saveData('most-recent-deletion', response.data.id)
+}
+
+function closeModal() {
+   const deleteModal = document.getElementById('delete-modal')
+   deleteModal.classList.remove('open')
 }
